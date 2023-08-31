b1e11b160198d7ef9b0ec311fe3384afa7260d09
aliyansyed <aliyans@hiof.no>
2023-08-31 12:36:28 +0200

    variabler

--- back.js
+++ back.js
@@ -1,2 +1,9 @@
 let navn = "ali";
-console.log(navn)
+console.log(navn); 
+
+const newheadline = "variables cheat sheet";
+console.log(newheadline)
+// finn html element med id headline, og bytt ut veriden.
+document.getElementById("headline").innerHTML = newheadline;
+const newStatement = 10 + 12
+document.querySelectorAll("p").innerHTML = newStatement
